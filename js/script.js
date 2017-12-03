943695f2ce0d120add4103cde8e23f60935f2715
VickyMutai <vicky.mutai96@gmail.com>
2017-12-03 17:11:20 +0300

    Add dark and white theme

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $("button#dark").click(function(){
+      $("body").removeClass();
+      $("body").addClass("black-background");
+  });
+  $("button#light").click(function(){
+      $("body").removeClass();
+      $("body").addClass("white-background");
+  });
+});
